4648596dd657baf066fd62ecb61e42a66a1a970b
fufako <patryko.mikiciuk@gmail.com>
2022-08-01 14:18:42 +0200

    Create Task class with basic methods

---
+++ src/tasks.js
@@ -0,0 +1,22 @@
+class Task {
+  constructor(name, dueDate, isImportant) {
+    this.name = name
+    this.dueDate = dueDate
+    this.isImportant = isImportant
+  }
+  setName(name) {
+    this.name = name
+  }
+
+  getName() {
+    return this.name
+  }
+
+  setDate(dueDate) {
+    this.dueDate = dueDate
+  }
+
+  getDate() {
+    return this.dueDate
+  }
+}
