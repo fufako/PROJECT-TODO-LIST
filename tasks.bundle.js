df1c73133f21ac9437777ac67bdbeec96c6fcd01
fufako <patryko.mikiciuk@gmail.com>
2022-08-01 14:55:07 +0200

    Add project form to UI

--- tasks.bundle.js
+++ tasks.bundle.js
@@ -6,6 +6,28 @@
   \**********************/
 /***/ (() => {
 
+class Task {
+  constructor(name, dueDate, isImportant) {
+    this.name = name
+    this.dueDate = dueDate
+    this.isImportant = isImportant
+  }
+  setName(name) {
+    this.name = name
+  }
+
+  getName() {
+    return this.name
+  }
+
+  setDate(dueDate) {
+    this.dueDate = dueDate
+  }
+
+  getDate() {
+    return this.dueDate
+  }
+}
 
 
 /***/ })
@@ -16,3 +38,4 @@
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/tasks.js"));
 /******/ }
 ]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGFza3MuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOztBQUVBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBO0FBQ0E7O0FBRUE7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0LXRvZG8tbGlzdC8uL3NyYy90YXNrcy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjbGFzcyBUYXNrIHtcbiAgY29uc3RydWN0b3IobmFtZSwgZHVlRGF0ZSwgaXNJbXBvcnRhbnQpIHtcbiAgICB0aGlzLm5hbWUgPSBuYW1lXG4gICAgdGhpcy5kdWVEYXRlID0gZHVlRGF0ZVxuICAgIHRoaXMuaXNJbXBvcnRhbnQgPSBpc0ltcG9ydGFudFxuICB9XG4gIHNldE5hbWUobmFtZSkge1xuICAgIHRoaXMubmFtZSA9IG5hbWVcbiAgfVxuXG4gIGdldE5hbWUoKSB7XG4gICAgcmV0dXJuIHRoaXMubmFtZVxuICB9XG5cbiAgc2V0RGF0ZShkdWVEYXRlKSB7XG4gICAgdGhpcy5kdWVEYXRlID0gZHVlRGF0ZVxuICB9XG5cbiAgZ2V0RGF0ZSgpIHtcbiAgICByZXR1cm4gdGhpcy5kdWVEYXRlXG4gIH1cbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
